0ecaa983b1c5efbb0b12178e2333b9b50e54a7d6
ramon <pedroramon.r.lima@gmail.com>
2024-09-10 18:39:49 -0300

    Fix - Implementação do Cors

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const ncmRoutes = require('./routes/ncmRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Configuração básica do CORS para permitir todas as origens
+app.use(cors());
+
 // Middleware para processar JSON
 app.use(express.json());
+
+
 
 // Rotas
 app.use('/auth', authRoutes);
